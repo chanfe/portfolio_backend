6a5d324bfe286b208fde6ba356bdf9753e059e6a
Felix Chan <felixchan29@gmail.com>
2019-09-14 01:26:41 -0400

    added Crud

--- backend/app/controllers/awards_controller.rb
+++ backend/app/controllers/awards_controller.rb
@@ -1,13 +1,41 @@
 class AwardsController < ApplicationController
   def index
+    @awardss = Award.all
+    render json: @awardss
   end
 
   def show
+    @awards = Award.find(params[:id])
+    render json: @awards
   end
 
   def create
+    @awards = Award.create(strong_params)
+    render json: @awards, status: 201
   end
 
-  def delete
+  def update
+    @awards = Award.find(params[:id])
+    unless @awards.nil?
+      @awards.update(strong_params)
+      render json: @awards
+    else
+      render json: { error: "Award not Found!" }, status: 404
+    end
+  end
+
+  def destroy
+    @awards = Award.find(params[:id])
+    unless @awards.nil?
+      @awards.destroy
+      render json: @awards
+    else
+      render json: { error: "Award not Found!" }, status: 404
+    end
+  end
+
+  private
+  def strong_params
+    params.require(:awards).permit(:id, :name, :demo, :image, :github)
   end
 end
--- backend/app/controllers/projects_controller.rb
+++ backend/app/controllers/projects_controller.rb
@@ -1,13 +1,42 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = Project.all
+    render json: @projects
   end
 
   def show
+    @project = Project.find(params[:id])
+    render json: @project
   end
 
   def create
+    @project = Project.create(strong_params)
+    render json: @project, status: 201
   end
 
-  def delete
+  def update
+    @project = Project.find(params[:id])
+    unless @project.nil?
+      @project.update(strong_params)
+      render json: @project
+    else
+      render json: { error: "Project not Found!" }, status: 404
+    end
   end
+
+  def destroy
+    @project = Project.find(params[:id])
+    unless @project.nil?
+      @project.destroy
+      render json: @project
+    else
+      render json: { error: "Project not Found!" }, status: 404
+    end
+  end
+
+  private
+  def strong_params
+    params.require(:project).permit(:id, :name, :demo, :image, :github)
+  end
+
 end
