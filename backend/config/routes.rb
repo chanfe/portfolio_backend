70f2fa2f0fdedeeab3fbed7797f6efa431c66071
Felix Chan <felixchan29@gmail.com>
2019-09-15 16:45:05 -0400

    routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'awards/index'
-  get 'awards/show'
-  get 'awards/create'
-  get 'awards/delete'
-  get 'projects/index'
-  get 'projects/show'
-  get 'projects/create'
-  get 'projects/delete'
+  resource :awards
+  resource :projects
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
